d0d40dde775ab9f24bdf9c220f128306cc1c8fcf
Avdasov Oleksandr <agentfreedom@zoho.com>
2023-12-26 01:35:31 +0200

    feat: add constructor for stack

--- src/stack.js
+++ src/stack.js
@@ -13,6 +13,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
+	constructor() {
+    this.array = [];
+	}
+	
   push(/* element */) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
