2e5ce5b31a4262f2d6253488cbc367618bb2da17
Avdasov Oleksandr <agentfreedom@zoho.com>
2023-12-25 15:31:33 +0200

    feat: add constructor for rootElem and function addElem in node-tree with distribution to the right and left + fix: rename rootElem and uncomment constructor new Node for add list-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,22 +1,44 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+	constructor() {
+    this.rootElem = null;
+  }
 
   root() {
     // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-		return this.root = null;
+		return this.rootElem;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  add(data) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+		this.rootElem = addElem(this.rootElem, data);
+
+		function addElem(node, data) {
+			if (!node) {
+				return new Node(data);
+			}
+
+			if (node.data === data) {
+				return node;
+			}
+
+			if (data < node.data) {
+				node.left = addElem(node.left, data);
+			} else {
+				node.right = addElem(node.right, data);
+			}
+
+			return node;
+		}
   }
 
   has(/* data */) {
