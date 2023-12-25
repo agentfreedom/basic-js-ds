99a60620c9f124f8b74d41be74d7b8dc4b55ea47
Avdasov Oleksandr <agentfreedom@zoho.com>
2023-12-26 01:54:11 +0200

    feat: add getUnderlyingList method for queue

--- src/queue.js
+++ src/queue.js
@@ -20,8 +20,9 @@ class Queue {
   }
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+		return this.first;
   }
 
   enqueue(/* value */) {
